c31447c11b91e1c49b71775030bb5c95f0fccc1a
marsur <martin.suraba@gmail.com>
2022-09-16 15:55:32 +0200

    Create check_cron_and_restore.sh

---
+++ check_cron_and_restore.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#checking and restoring crontab
+
+# check option: crontab -ir
+
+if test -e /var/spool/cron/root
+
+then
+
+echo my heart belongs to toilet paper ..... (I will explain... I promise)
+
+echo "I'm waiting for girls very close to the wc, mlask"  > /dev/null
+
+else
+
+cat /root/backup_cron.txt > /var/spool/cron/root
+
+fi
